1001484f5d89a66bb77ca22467571d7219b09e56
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2019-10-23 01:43:50 +0300

    Create a method for updating a subset of map values

--- utils.js
+++ utils.js
@@ -1,11 +1,17 @@
 const { List, Map } = require("immutable");
 
-const invert = (map) => (
-    map.reduce((res, val, key) => (
+function invert(map) {
+    return map.reduce((res, val, key) => (
         res.has(val)
             ? res.update(val, keys => keys.push(key))
             : res.set(val, List.of(key))
     ), Map())
-);
+}
 
-module.exports = { invert };
+function updateAll(map, keys, updater) {
+    return keys.reduce((map, index) => (
+        map.update(index, value => updater(value))
+    ), map);
+}
+
+module.exports = { invert, updateAll };
